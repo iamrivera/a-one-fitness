a81e9dd521d3e47761b8423f353aa970e60d0e0d
iamrivera <a3m3rivera@gmail.com>
2021-06-01 08:46:59 -0700

    Views updates and Session Controller Helpers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+    protect_from_forgery 
+    helper_method :current_user, :logged_in?
+
+    def logged_in? 
+        session[:id].present?
+    end
+
+    def current_user
+        @current_user = User.find(session[:id])
+        @current_user
+    end
+    
 end
