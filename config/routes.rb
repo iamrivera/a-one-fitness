f82d0cd9f23db683319fb8ac0a380fda8589a31f
iamrivera <a3m3rivera@gmail.com>
2021-06-01 08:43:34 -0700

    Routes creation

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :exercises
+  resources :workouts
+  resources :trainers
+  resources :users
 
-  
+  get 'signup', to: 'users#new', as: :signup #this gives us path helper
+  get 'login', to: 'sessions#new', as: :login 
+
+  get 'logout', to: 'sessions#destroy', as: :logout 
+
+  root to: "workouts#index", as: :home
+
 end
